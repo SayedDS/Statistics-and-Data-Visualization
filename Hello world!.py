fb41580533fa716f7813067eac9300b642df9c3c
Sayed Mohamed Alalawi <alalawi.dsba@gmail.com>
2024-11-19 20:45:58 +0300

    Create Hello world!.py

---
+++ Hello world!.py
@@ -0,0 +1,13 @@
+# Hello guys ! 
+
+print("Welcome to my repository !")
+
+# This is my first file to create in github !\
+
+
+# I'm a student in data science. I created this repository to share my learning and my projects with you ! 
+
+# I hope you will enjoy it ! 
+
+
+
